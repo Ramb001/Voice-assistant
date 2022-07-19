6c8cf5786b1a3ed78351f00d68b90fb47a860fd3
Artem <artem.rmb@bk.ru>
2022-07-19 07:15:16 +0300

    Add new key to commands dict

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import speech_recognition
-from commands import openVk, openYoutube, playMusic
+from commands import openVk, openYoutube, playMusic, greeting
+
 
 sr = speech_recognition.Recognizer()
 sr.pause_threshold = 0.5
+
 
 commandsDict = {
     'commands' : {
         'playMusic': 'включи музыку',
         'openVk' : ['открой вк', 'включи вк', 'включи vk', 'открой vk'],
-        'openYoutube': ['открой youtube', 'включи youtube']
+        'openYoutube': ['открой youtube', 'включи youtube'],
+        'greeting': ['привет', 'здарова', 'добрый день']
     }
 }
+
 
 def listenCommand():
     try:
         with speech_recognition.Microphone() as mic:
-            sr.adjust_for_ambient_noise(source = mic, duration = 0.5)
+            sr.adjust_for_ambient_noise(source=mic, duration=0.5)
             audio = sr.listen(source=mic)
             query = sr.recognize_google(audio_data=audio, language='ru-Ru'). lower()
         return query
