eab1d56fe5a854a2637523ac1c16009f0a604941
Artem <artem.rmb@bk.ru>
2022-07-19 06:46:29 +0300

    Add new Files

---
+++ commands.py
@@ -0,0 +1,10 @@
+import os
+
+def playMusic():
+    os.system('open /System/Applications/Music.app')
+
+def openVk():
+    os.system('open https://vk.com')
+
+def openYoutube():
+    os.system('open https://youtube.com')    
---
+++ main.py
@@ -0,0 +1,36 @@
+import speech_recognition
+from commands import openVk, openYoutube, playMusic
+
+sr = speech_recognition.Recognizer()
+sr.pause_threshold = 0.5
+
+commandsDict = {
+    'commands' : {
+        'playMusic': 'включи музыку',
+        'openVk' : ['открой вк', 'включи вк', 'включи vk', 'открой vk'],
+        'openYoutube': ['открой youtube', 'включи youtube']
+    }
+}
+
+def listenCommand():
+    try:
+        with speech_recognition.Microphone() as mic:
+            sr.adjust_for_ambient_noise(source = mic, duration = 0.5)
+            audio = sr.listen(source=mic)
+            query = sr.recognize_google(audio_data=audio, language='ru-Ru'). lower()
+        return query
+    
+    except speech_recognition.UnknownValueError:
+        return 'Повторите ещё раз'
+
+
+def main():    
+    query = listenCommand()
+
+    for k, v in commandsDict['commands'].items():
+        if query in v:
+            print(globals()[k]())
+        
+
+if __name__ == '__main__':
+    main()
